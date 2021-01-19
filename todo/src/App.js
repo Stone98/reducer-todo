2edab7225fd2a4dd1243b3859ed1905e113e4537
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-01-19 15:54:20 -0700

    set up ui set up

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <h1>Todo App</h1>
+      <div className="todoList">
+        <h3>List of Items</h3>
+        <ul>
+          <li>Todo 1</li>
+          <li>Todo 2</li>
+          <li>Todo 3</li>
+        </ul>
+        <button>Clear Completed</button>
+      </div>
+      <div className="todoForm">
+        <form>
+          <label htmlFor="">
+            Title:
+             <input type="text" />
+          </label>
+          <button>Submit Todo</button>
+        </form>
+      </div>
     </div>
   );
 }
