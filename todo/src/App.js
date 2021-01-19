e39feb4782810eb31ca82736606dca12be07bac7
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-01-19 15:57:01 -0700

    injesting todo data

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,18 +3,39 @@ import React from 'react';
 
 
 function App() {
+  const todos = [{
+    title: 'todo 1',
+    id: 0,
+    completed: false
+  },
+  {
+    title: 'todo 2',
+    id: 1,
+    completed: false
+  },
+  {
+    title: 'todo 3',
+    id: 2,
+    completed: false
+  },
+  ]
   return (
     <div>
       <h1>Todo App</h1>
       <div className="todoList">
         <h3>List of Items</h3>
         <ul>
-          <li>Todo 1</li>
-          <li>Todo 2</li>
-          <li>Todo 3</li>
+          {
+            todos.map(todo => {
+              return (
+                <li>{todo.title}</li>
+              )
+            })
+          }
         </ul>
         <button>Clear Completed</button>
       </div>
+
       <div className="todoForm">
         <form>
           <label htmlFor="">
